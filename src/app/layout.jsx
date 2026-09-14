b050bae8e76a53b2a33b0b8634e582b36d204357
dolodorsey <dolodorsey@gmail.com>
2026-09-14 02:30:25 -0400

    style: apply locked STUSH visual standard last

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -5,6 +5,7 @@ import './experience.css';
 import './stush-fixes.css';
 import './stush-pages-v2.css';
 import './production-refinement.css';
+import './stush-visual-standard.css';
 import { Bodoni_Moda, DM_Mono, DM_Sans } from 'next/font/google';
 import MobileMenu from '@/components/MobileMenu';
 import NavScroll from '@/components/NavScroll';

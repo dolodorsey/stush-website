f168752b5ea9a9ba187ad55b5719207e82b19777
dolodorsey <dolodorsey@gmail.com>
2026-09-14 04:11:31 -0400

    Load final STUSH visual QA overrides

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -7,6 +7,7 @@ import './stush-pages-v2.css';
 import './production-refinement.css';
 import './stush-visual-standard.css';
 import './category-editorial.css';
+import './qa-fixes.css';
 import { Bodoni_Moda, DM_Mono, DM_Sans } from 'next/font/google';
 import MobileMenu from '@/components/MobileMenu';
 import NavScroll from '@/components/NavScroll';

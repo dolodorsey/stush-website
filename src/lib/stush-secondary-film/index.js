777e14329b033e9bb72125c8023abd27e743d8a6
dolodorsey <dolodorsey@gmail.com>
2026-09-14 21:02:04 -0400

    Assemble STUSH secondary house film

---
+++ src/lib/stush-secondary-film/index.js
@@ -0,0 +1,12 @@
+import p0 from './part0';
+import p1 from './part1';
+import p2 from './part2';
+import p3 from './part3';
+import p4 from './part4';
+import p5 from './part5';
+import p6 from './part6';
+import p7 from './part7';
+
+// Ambient in-page derivative of the user-supplied STUSH ANI(3) film.
+// Never use this as the homepage hero; /STUSH_VID.mp4 remains the locked homepage film.
+export const STUSH_SECONDARY_FILM = `data:video/mp4;base64,${p0}${p1}${p2}${p3}${p4}${p5}${p6}${p7}`;

85cd9a0c3029dbe3dcfe4d4dce5f6d9cddb073e0
dolodorsey <dolodorsey@gmail.com>
2026-09-15 00:36:57 -0400

    Load STUSH density overrides last

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -11,6 +11,7 @@ import './qa-fixes.css';
 import './campaign-refresh.css';
 import './house-world-v2.css';
 import './release-qa-fixes.css';
+import './density-pass.css';
 import { Bodoni_Moda, DM_Mono, DM_Sans } from 'next/font/google';
 import MobileMenu from '@/components/MobileMenu';
 import NavScroll from '@/components/NavScroll';

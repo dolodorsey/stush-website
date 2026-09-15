522886eea8051836f4a9ff86f97a77b732db0df6
dolodorsey <dolodorsey@gmail.com>
2026-09-14 23:30:37 -0400

    Load final lookbook QA fix after house-world styles

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -10,6 +10,7 @@ import './category-editorial.css';
 import './qa-fixes.css';
 import './campaign-refresh.css';
 import './house-world-v2.css';
+import './release-qa-fixes.css';
 import { Bodoni_Moda, DM_Mono, DM_Sans } from 'next/font/google';
 import MobileMenu from '@/components/MobileMenu';
 import NavScroll from '@/components/NavScroll';

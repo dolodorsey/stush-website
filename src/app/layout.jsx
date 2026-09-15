23528bfde820df863e3618502b3c9aacc938cbd9
dolodorsey <dolodorsey@gmail.com>
2026-09-14 21:07:06 -0400

    Load STUSH house-world UI layer last

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -9,6 +9,7 @@ import './stush-visual-standard.css';
 import './category-editorial.css';
 import './qa-fixes.css';
 import './campaign-refresh.css';
+import './house-world-v2.css';
 import { Bodoni_Moda, DM_Mono, DM_Sans } from 'next/font/google';
 import MobileMenu from '@/components/MobileMenu';
 import NavScroll from '@/components/NavScroll';
